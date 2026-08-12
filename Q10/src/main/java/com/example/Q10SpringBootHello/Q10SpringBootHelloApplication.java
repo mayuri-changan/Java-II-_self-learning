3f9a175fb2238ebecc740a376038697eb9b760b7
mayuri-changan <mayuri2001changan@gmail.com>
2026-08-12 22:30:42 +0530

    Q10

---
+++ Q10/src/main/java/com/example/Q10SpringBootHello/Q10SpringBootHelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.Q10SpringBootHello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q10SpringBootHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q10SpringBootHelloApplication.class, args);
+	}
+
+}
